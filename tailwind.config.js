e9bd85b974a784c7bf46e084c1f529897a687e06
Success Obasi <obasisuccess192@gmail.com>
2023-09-30 05:42:14 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'epilogue': ['Epilogue', 'sans-serif'],
+      
+      },
+    },
+  },
+  plugins: [],
+}
+
